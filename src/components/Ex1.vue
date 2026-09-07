e68ce0afe279a507327bc25d7561274c098bd01a
rachellim04 <s10220886@connect.np.edu.sg>
2026-09-07 10:29:14 +0800

    Completed

--- src/components/Ex1.vue
+++ src/components/Ex1.vue
@@ -2,17 +2,30 @@
     import { ref } from 'vue'
 
     // Add code here
+    const message = ref('Choose your favourite fruit:');
+
+    // const fruit1 = ref('apple');
+    // const fruit2 = ref('orange');
+    // const fruit3 = ref('peach');
+
     // challenge: use array  
+    const fruits = ref(['apple', 'orange', 'peach']);
     
 </script>
 
 <template>
     {{ message }} 
    
+    <!--
     <label for=""> <input type="radio" name="fruit"> {{ fruit1 }} </label> 
     <label for=""> <input type="radio" name="fruit"> {{ fruit2 }} </label> 
     <label for=""><input type="radio" name="fruit"> {{ fruit3 }} </label> 
-    
+    -->
+
+    <!-- using of array -->
+    <label v-for="fruit in fruits" :key ="fruit">
+        <input type="radio" name="fruit" > {{ fruit }}
+    </label>
 </template>
 
 <style scoped>
--- src/components/Ex2.vue
+++ src/components/Ex2.vue
@@ -3,10 +3,23 @@
 
     // Add code here
 
+    // single attribute binding
+    const width = ref('700');
+    const height = ref('400');
+    const image = ref('/assets/me.png');
+
+    // multiple attribute binding
+    const size = ref({
+        width: '700',
+        height: '400',
+        src: '/assets/me.png'
+    })
 </script>
 
 <template>
 
     <!-- Add code here -->
+     <img v-bind:src="image" :width="width" :height="height" />
    
+    <img v-bind="size" />
 </template>
